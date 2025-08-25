af18344708b1f59ccff106e09d10c6b2936bc3eb
cleanSumo <johannes@karabin.se>
2025-08-25 16:20:09 +0200

    panel name change

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -2,7 +2,7 @@
     Create a panel, and add listeners for panel show/hide events.
 */
 browser.devtools.panels.create(
-    'Inertia.js',
+    'Inertia 🕵️‍♂️',
     '/icons/inertia.svg',
     '/devtools/panel/panel.html',
 )
